f0292b11800862ac40e216be5b2682aa86864a49
孙笑闯 <460046653@qq.com>
2021-05-25 12:29:04 +0800

    frist commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    title: "FE-interview",
+    description: "HTML CSS JavaScript Vue React",
+    base: '/',
+
+
+
+    themeConfig: {
+        //   头部导航
+        nav: [{
+            text: '首页',
+            link: '/'
+        }, ],
+        //   侧边导航
+        sidebar: [{
+                text: 'HTML',
+                link: '/html/',
+                
+            },
+            {
+                text: 'CSS',
+                link: '/css/'
+            },
+            {
+                text: 'JavaScript',
+                link: '/javascript/'
+            },
+            {
+                text: 'Vue',
+                link: '/vue/'
+            },
+
+        ]
+    }
+}
